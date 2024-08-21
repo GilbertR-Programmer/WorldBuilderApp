773b9107bf8e1c54101ce39615e0813e177521c1
Gilbert Reid <greid@spartaglobal.com>
2024-08-21 14:05:50 +0100

    initial commit

---
+++ WorldBuilderApi/src/main/java/com/sparta/grr/worldbuilderapi/WorldBuilderApiApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.grr.worldbuilderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldBuilderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldBuilderApiApplication.class, args);
+    }
+
+}
---
+++ WorldBuilderApi/src/test/java/com/sparta/grr/worldbuilderapi/WorldBuilderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.grr.worldbuilderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldBuilderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
